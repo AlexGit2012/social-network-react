7452ad96cb98d760d52e11a6ba71035f5ce5b408
AlexGit2012 <a.karavaychik@gmail.com>
2020-03-17 20:56:58 +0300

    - added tests for ProfileStatus for input and callback

--- social-network-project/src/App.test.js
+++ social-network-project/src/App.test.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {Provider} from "react-redux";
-import store from "./Components/Redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
 import SamuraiJSApp from "./App";
 
 it('renders without crashing', () => {
--- social-network-project/src/Components/Profile/ProfileStatus.test.jsx
+++ social-network-project/src/Components/Profile/ProfileStatus.test.jsx
@@ -13,13 +13,39 @@ describe("ProfileStatus component", ()=>{
         const component = create(<ProfileStatus status="it-kamasutra.com" />)
         const root = component.root;
         let span = root.findByType("span");
-        expect(span.length).toBe(1);
+        expect(span).not.toBeNull();
+    });
+
+    test("after creation 'input' shouldn't be displayed", ()=>{
+        const component = create(<ProfileStatus status="it-kamasutra.com" />)
+        const root = component.root;
+
+        expect(()=>{
+            let input = root.findByType("input");
+        }).toThrow();
     });
 
     test("after creation 'span' should contain correct status", ()=>{
         const component = create(<ProfileStatus status="it-kamasutra.com" />)
         const root = component.root;
         let span = root.findByType("span");
-        expect(span.length).toBe(1);
+        expect(span.children[1]).toBe("it-kamasutra.com");
+    });
+
+    test("input should be displayed in editMode", ()=>{
+        const component = create(<ProfileStatus status="it-kamasutra.com" />)
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        let input = root.findByType("input")
+        expect(input.props.value).toBe("it-kamasutra.com");
+    });
+
+    test("callback should be called", ()=>{
+        const mockCallBack = jest.fn();
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateUserStatus={mockCallBack}/>)
+        const instance = component.getInstance();
+        instance.deactivateEditMode();
+        expect(mockCallBack.mock.calls.length).toBe(1);
     });
 });
